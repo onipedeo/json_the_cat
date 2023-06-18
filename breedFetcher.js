90633167e20b99e0b97cf97f79220a4faab33745
Tobi Onipede <onipede89@gmail.com>
2023-06-19 00:23:10 +0100

    correct bug with some some breed coming back as not existing

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,25 +1,28 @@
 const request = require("request");
+
 //collect the CLI argument
 const args = process.argv.slice(2);
+const breedName = args[0];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[0]}`, (error, response, body) => {
+
+request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
   //catch error of broken link
   if (error) {
     console.log('There is an error: ', error);
   }
+  //IF status is not 200, then there is trouble
+  if (response.statusCode !== 200) {
+    console.log('There is an issue that needs your attention');
+  }
   //converting my data to an array and object
   const data = JSON.parse(body);
   const dataObj = data[0];
-  //Initialise array for iteration to check if the breed exists
-  const valueArr = [];
-  for (const b in dataObj) {
-    //push all vales to the value array
-    valueArr.push(dataObj[b]);
-    //check if value array has the breed ID/initials.
-    if (valueArr.includes(args[0])) {
-      return console.log('Description: ', dataObj.description);
-    }
-  }
-  return console.log(`${args[0]} breed doesn't exist`);
 
+  //check if the breed doesn't exist
+  if (data.length === 0) {
+    console.log(`${breedName} breed doesn't exist`);
+    return;
+  }
+  console.log('Description: ', dataObj.description);
 });
+
